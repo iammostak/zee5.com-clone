02a9a42eaacf3ca4e6789cd66e9312516b8ceeb1
iammostak <mdmostafijur85@gmail.com>
2022-07-21 02:20:52 +0530

    day-2 - navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Footer />
     </div>
   );
 }
