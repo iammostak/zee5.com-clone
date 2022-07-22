56dc3ae4eb26f9ba851ad73c0153d44e16d75231
iammostak <mdmostafijur85@gmail.com>
2022-07-22 23:13:35 +0530

    day-4

---
+++ src/Components/TopTen.jsx
@@ -0,0 +1,36 @@
+import { Box, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FaCrown } from "react-icons/fa";
+
+function TopTen() {
+   const [data, setData] = useState([]);
+
+   useEffect(() => {
+      axios.get(`https://json-server-mock-api-all.herokuapp.com/zeeTop10`)
+         .then((res) => {
+            setData(res.data);
+         });
+   }, []);
+
+   return (
+      <Box m={10} textAlign='left'>
+         <Text fontSize='2xl' fontWeight='bold'>Top 10 in India</Text>
+         <Grid templateColumns='repeat(5, 1fr)' gap={7} my={4}>
+            {
+               data.map((item, index) => (
+                  <GridItem key={index} cursor='pointer' position='relative'>
+                     <Image src={item.url} alt={item.popupTitle} borderRadius='md' w='100%' />
+                     <Box className='card-icon' top='7px' left='7px'>
+                        <FaCrown />
+                     </Box>
+                     <Text className='top-ten-numbers'>{item.top10Numbers}</Text>
+                  </GridItem>
+               ))
+            }
+         </Grid>
+      </Box>
+   );
+}
+
+export default TopTen;
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,10 +1,22 @@
 import { Box } from "@chakra-ui/react";
+import IMDBtopPicks from "../Components/IMDBtopPicks";
+import LatestMovies from "../Components/LatestMovies";
+import LeadingLadies from "../Components/LeadingLadies";
+import OriginalWeb from "../Components/OriginalWeb";
+import PremiumBollywood from "../Components/PremiumBollywood";
 import Slideshow from "../Components/Slideshow";
+import TopTen from "../Components/TopTen";
 
 function Home() {
    return (
       <Box my={7}>
          <Slideshow />
+         <TopTen />
+         <LatestMovies />
+         <OriginalWeb />
+         <IMDBtopPicks />
+         <LeadingLadies />
+         <PremiumBollywood />
       </Box>
    );
 }
